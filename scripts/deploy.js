555625d25b61c89587192025e49d37532e64e8f8
Shr2405 <mishrasupratik81@gmail.com>
2024-09-27 17:42:00 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,178 +1,23 @@
-import {useState, useEffect} from "react";
-import {ethers} from "ethers";
-import atm_abi from "../artifacts/contracts/Assessment.sol/Assessment.json";
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
 
-export default function HomePage() {
-  const [ethWallet, setEthWallet] = useState(undefined);
-  const [account, setAccount] = useState(undefined);
-  const [atm, setATM] = useState(undefined);
-  const [Speeding, setBalance] = useState(undefined);
+async function main() {
+  const initBalance = 1;
+  const Assessment = await hre.ethers.getContractFactory("Assessment");
+  const assessment = await Assessment.deploy(initBalance);
+  await assessment.deployed();
 
-  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-  const atmABI = atm_abi.abi;
-
-  const getWallet = async() => {
-    if (window.ethereum) {
-      setEthWallet(window.ethereum);
-    }
-    if (ethWallet) {
-      const account = await ethWallet.request({method: "eth_accounts"});
-      handleAccount(account);
-    }
-  }
-
-  const handleAccount = (account) => {
-    if (account) {
-      console.log ("Account connected: ", account);
-      setAccount(account);
-    }
-    else {
-      console.log("No account found");
-    }
-  }
-
-  const connectAccount = async() => {
-    if (!ethWallet) {
-      alert('MetaMask wallet is required to connect');
-      return;
-    }
-  
-    const accounts = await ethWallet.request({ method: 'eth_requestAccounts' });
-    handleAccount(accounts);
-    
-    // once wallet is set we can get a reference to our deployed contract
-    getATMContract();
-  };
-
-  const getATMContract = () => {
-    const provider = new ethers.providers.Web3Provider(ethWallet);
-    const signer = provider.getSigner();
-    const atmContract = new ethers.Contract(contractAddress, atmABI, signer);
- 
-    setATM(atmContract);
-  }
-
-  const getBalance = async() => {
-    if (atm) {
-      setBalance((await atm.getBalance()).toNumber());
-    }
-  }
-
-  const IncreaseSpeed = async() => {
-    if (atm) {
-      const Speed = document.getElementById("amount").value;
-      let tx = await atm.Incresespeed(Speed);
-      await tx.wait()
-      getBalance();
-    }
-  }
-
-  const DecreaseSpeed = async() => {
-    if (atm) {
-      const Speed = document.getElementById("amount").value;
-      let tx = await atm.Negativerating(Speed);
-      await tx.wait()
-      getBalance();
-    }
-  }
-
-  const initUser = () => {
-    // Check to see if user has Metamask
-    if (!ethWallet) {
-      return <p>Please install Metamask in order to use this Speeding System.</p>
-    }
-
-    // Check to see if user is connected. If not, connect to their account
-    if (!account) {
-      return <button onClick={connectAccount}>Please connect your Metamask wallet</button>
-    }
-
-    if (Speeding == undefined) {
-      getBalance();
-    }
-
-return (
-  <div className="h-full flex flex-col items-center justify-center">
-     <h6 className="mt-2 mb-40 border-2 border-neutral-600 border-neutral-500 p-1 rounded text-xl">
-      <p>Fan : {account}</p>
-      </h6>
-      <h6 className="flex justify-center font-black text-5xl">
-      <p>Speed : {Speeding}</p>
-      </h6>
-     
-      <div className="flex items-center justify-evenly">
-          <button className="btn m-2" onClick={IncreaseSpeed}>
-          Increase
-          </button>
-          <button className="btn m-2" onClick={DecreaseSpeed}>
-          Decrease
-          </button>
-      </div>
-      <form className="flex items-center justify-center">
-          <label className="text-xl me-1">Speed :</label>
-          <input
-              id="amount"
-              type="number"
-              className="bg-transparent border-2 border-neutral-600 p-1 rounded text-xl w-14"
-              defaultValue={1}
-              min={1}
-          />
-      </form>
-  </div>
-);
-};
-
-useEffect(() => {
-getWallet();
-}, []);
-
-return (
-<main className="w-screen h-screen flex items-center justify-evenly">
-  <script src="https://cdn.tailwindcss.com"></script>
-  <link
-      href="https://cdn.jsdelivr.net/npm/daisyui@3.9.4/dist/full.css"
-      rel="stylesheet"
-      type="text/css"
-  />
-  <link
-      href="https://fonts.cdnfonts.com/css/chicago-2"
-      rel="stylesheet"
-  ></link>
-  <link
-      href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
-      rel="stylesheet"
-  ></link>
-
-  <div className="grid grid-cols-2 gap-63">
-      <header className="col-span-1 flex items-center">
-          <h1 className="font-normal text-6xl text-yellow-400">
-          Fan speed controller
-          </h1>
-      </header>
-
-      <section className="col-span-1 h-screen flex justify-center items-center">
-          {initUser()}
-      </section>
-  </div>
-  <style jsx>
-      {`
-          * {
-              font-family: "Roboto", sans-serif;
-          }
-
-          body {
-              background-color: #1a202c;
-          }
-
-          h1 {
-              font-family: "Chicago", sans-serif;
-          }
-
-          .container {
-              text-align: center;
-          }
-      `}
-  </style>
-</main>
-);
+  console.log(`A contract with balance of ${initBalance} eth deployed to ${assessment.address}`);
 }
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
